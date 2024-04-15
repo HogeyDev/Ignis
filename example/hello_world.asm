f72ad8e1e1269140146dc34c709479f8f03b7508
Evan Hoge <evanhoge123@gmail.com>
2024-04-15 07:42:08 -0400

    Changed some assembly

--- example/hello_world.asm
+++ example/hello_world.asm
@@ -1,85 +1,11 @@
 section .text
-global putchar
-putchar:
+global exit
+exit:
 	push rbp
 	mov rbp, rsp
-	mov rax, 1
-	mov rdi, 1
-	movzx rsi, byte [rbp+16]
-	mov rdx, 1
+	mov rax, 60
+	mov rdi, qword [rbp+16]
 	syscall
-	mov rsp, rbp
-	pop rbp
-	ret
-global strlen
-strlen:
-	push rbp
-	mov rbp, rsp
-	mov rsi, qword [rsp+16]
-	mov rax, -1
-	.looper:
-	inc rax
-	cmp byte [rsi+rax], 0x00
-	jne .looper
-	mov rsp, rbp
-	pop rbp
-	ret
-global print
-print:
-	push rbp
-	mov rbp, rsp
-	sub rsp, 8
-	mov qword [rbp-8], 0
-	push 0 ; integer literal
-	pop qword rax
-	mov qword [rbp-8], rax ; assigned `i`
-lbl0:
-	mov rax, qword [rbp+16]
-	push rax ; recalled `str`
-	mov rax, qword [rbp-8]
-	push rax ; recalled `i`
-	pop qword rbx
-	pop qword rax
-	mov rax, [rax + rbx]
-	push rax
-	pop qword rax
-	cmp rax, 0
-	je lbl1
-	mov rax, qword [rbp+16]
-	push rax ; recalled `str`
-	mov rax, qword [rbp-8]
-	push rax ; recalled `i`
-	pop qword rbx
-	pop qword rax
-	mov rax, [rax + rbx]
-	push rax
-	call putchar
-	add rsp, 8
-	mov rax, qword [rbp-8]
-	push rax ; recalled `i`
-	push 1 ; integer literal
-	pop qword rbx
-	pop qword rax
-	add rax, rbx
-	push rax
-	pop qword rax
-	mov qword [rbp-8], rax ; assigned `i`
-	jmp lbl0
-lbl1:
-	mov rsp, rbp
-	pop rbp
-	ret
-global println
-println:
-	push rbp
-	mov rbp, rsp
-	mov rax, qword [rbp+16]
-	push rax ; recalled `str`
-	call print
-	add rsp, 8
-	push 10 ; integer literal
-	call putchar
-	add rsp, 8
 	mov rsp, rbp
 	pop rbp
 	ret
@@ -87,8 +13,8 @@ global main
 main:
 	push rbp
 	mov rbp, rsp
-	push STR0
-	call println
+	push 1 ; integer literal
+	call exit
 	add rsp, 8
 	mov rsp, rbp
 	pop rbp
@@ -103,4 +29,3 @@ _start:
 	mov rdi, 0
 	syscall
 section .data
-	STR0 db "Hello, World!", 0
