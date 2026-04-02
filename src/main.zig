bc8cb62bc7361b888860751da740933ea2829d7a
Evan Hoge <evanhoge123@gmail.com>
2026-04-02 01:55:23 -0400

    tested cli parsing capabilities and added my favorite person

--- src/main.zig
+++ src/main.zig
@@ -13,8 +13,19 @@ pub fn main() !void {
     defer cli_parser.deinit();
 
     std.debug.print("flags:\n", .{});
-    var it = cli_parser.options.iterator();
-    while (it.next()) |entry| {
+    var flags_iter = cli_parser.flags.iterator();
+    while (flags_iter.next()) |entry| {
+        std.debug.print("\t{s}\n", .{ entry.key_ptr.* });
+    }
+    std.debug.print("options:\n", .{});
+    var options_iter = cli_parser.options.iterator();
+    while (options_iter.next()) |entry| {
         std.debug.print("\t{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
     }
+    std.debug.print("arguments:\n", .{});
+    for (cli_parser.arguments.items) |arg| {
+        std.debug.print("\t{s}\n", .{ arg });
+    }
+
+    try std.fs.File.stdout().writeAll("Hey, Nut!\n");
 }
